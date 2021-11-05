6fedb9fe66107f771c92a2faec7d45bbd3a8e59c
xiaoshen <gushi@qq.com>
2021-11-05 18:12:35 +0800

    first

---
+++ XLuaDemo/Assets/Editor/BuildAssetBundles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using UnityEditor;
+
+public class BuildAssetBundles : MonoBehaviour
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static public void BuildAllAssetBundles()
+    {
+        string dir = "AssetBundles";
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+
+        BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+        UnityEngine.Debug.Log("AssetBundle资源打包完成");
+        
+    }
+}
